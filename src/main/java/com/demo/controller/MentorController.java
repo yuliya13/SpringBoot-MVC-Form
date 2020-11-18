ff91b3cdca915fdd2202b8f5d5ae7f3c0641636d
iuliia <yuliyapgh@gmail.com>
2020-11-17 20:46:37 -0500

    added mvc form demo

---
+++ src/main/java/com/demo/controller/MentorController.java
@@ -0,0 +1,33 @@
+package com.demo.controller;
+
+import com.demo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+
+        model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("B7", "B8", "B9", "B10", "B11", "B12");
+        model.addAttribute("batchList", batchList);
+
+        return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+        System.out.println(mentor.toString());
+        return "mentor/mentor-confirmation";
+    }
+}
---
+++ src/main/java/com/demo/model/Mentor.java
@@ -0,0 +1,22 @@
+package com.demo.model;
+
+import lombok.*;
+
+import java.util.stream.Stream;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Mentor {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private boolean graduated;
+    private String batch;
+    private String company;
+
+}
